c92a444fb408cf71821e775704a2b1e7f4c9070c
IAMCHINMAYDWIVEDI <cd.19.06.2006@gmail.com>
2026-03-16 15:15:52 +0530

    Add palindrome checking functionality

--- Pal.java
+++ Pal.java
@@ -3,5 +3,18 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.");
+    
+     String word = "madam";
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        if (word.equals(reverse)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
